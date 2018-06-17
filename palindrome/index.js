dc1bf065ed912794ec139d669a5527215ae581f3
hugoisrr <84rami@gmail.com>
2018-06-17 21:06:28 +0200

    Problems Palindrome & reverseInt solved

--- palindrome/index.js
+++ palindrome/index.js
@@ -4,9 +4,47 @@
 // form the same word if it is reversed. *Do* include spaces
 // and punctuation in determining if the string is a palindrome.
 // --- Examples:
-//   palindrome("abba") === true
-//   palindrome("abcdefg") === false
+// === true
+console.log("Test abba: " + palindrome("abba"));
+console.log("Test abcdefg: " + palindrome("abcdefg")); // === false
+/* 
+3rd Solution
+This approach is more direct using every()
+The every() method tests whether all elements in the array pass the test implemented by the provided function.
+*/
+function palindrome(str) {
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
+/* 
+2nd Solution
+This a much direct approach, using JavaScript functions
+*/
+/* function palindrome(str) {
+  const reversed = str
+    .split("")
+    .reverse()
+    .join("");
 
-function palindrome(str) {}
+  return str === reversed;
+} */
+/* 
+1st Solution
+
+with a for loop we first reverse the string given,
+the we compare if the reversed string is equal to the 
+original string.
+*/
+/* function palindrome(str) {
+  let reversed = "";
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  if (reversed === str) return true;
+  else return false;
+} */
 
 module.exports = palindrome;
--- reverseint/index.js
+++ reverseint/index.js
@@ -2,12 +2,55 @@
 // Given an integer, return an integer that is the reverse
 // ordering of numbers.
 // --- Examples
-//   reverseInt(15) === 51
+console.log(reverseInt(-6789));
 //   reverseInt(981) === 189
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
+/* 
+2nd Solution
 
-function reverseInt(n) {}
+This approach is more direct
+*/
+function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  return parseInt(reversed) * Math.sign(n);
+}
+/* 
+1st Solution
+
+This approach extends more using if statements
+*/
+/* function reverseInt(n) {
+  let signInt = Math.sign(n);
+  let reversedInt = 0;
+
+  if (signInt == 1) {
+    return (reversedInt = parseInt(
+      n
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+    ));
+  } else if (signInt == -1) {
+    return (reversedInt = -Math.abs(
+      parseInt(
+        n
+          .toString()
+          .split("")
+          .reverse()
+          .join("")
+      )
+    ));
+  } else {
+    return 0;
+  }
+} */
 
 module.exports = reverseInt;
