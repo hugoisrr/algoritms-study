8c3947401cd9ff0d058c9c6df46557f582c9348a
hugoisrr <84rami@gmail.com>
2018-11-18 15:17:31 +0100

    Insert Node in LinkedList and show size of List

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,8 +2,43 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next=null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
 
+    insertFirst(data) {
+        // arguments passed are data; value store inside the Node, and next will store the location of the first node
+        const node = new Node(data, this.head);
+        // update the link to the new node
+        this.head = node;
+    }
+
+    size () {        
+        let counter = 0;
+        let node = this.head;
+
+        while (node) {
+            counter++;
+            node = node.next;
+        }
+
+        return counter;
+    }
+}
+
+const nodeOne = new Node(5);
+const list = new LinkedList();
+list.head = nodeOne;
+list.insertFirst(15);
+list
+
+console.log(list.size());
 module.exports = { Node, LinkedList };
